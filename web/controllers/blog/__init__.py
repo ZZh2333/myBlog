326b488a025db890a396109ff20148c83c01f3ba
ZZh2333 <13164108610@163.com>
2023-03-21 20:53:03 +0800

    update

--- web/controllers/blog/__init__.py
+++ web/controllers/blog/__init__.py
@@ -1,10 +1,20 @@
 from flask import Blueprint,render_template
+import pandas as pd
 
 route_blog = Blueprint('blog_page',__name__)
 
 from web.controllers.blog.steamcmdinstall import *
 from web.controllers.blog.maoxiandao import *
 
-@route_blog.route('/')
+@route_blog.route('/',methods=["Get","POST"])
 def index():
-    return render_template('blog/index.html')
+    data = pd.read_csv('web/static/blog/csv/flod.csv')
+    data['Date'] = pd.to_datetime(data['Date'])
+    data['Month'] = data.Date.apply(lambda x: x.month)
+    data['Day'] = data.Date.apply(lambda x: x.day)
+    data = data.sort_values(by='Date',ascending=False)
+    # print(data)
+    rs = []
+    for index,row in data.iterrows():
+        rs.append(row)
+    return render_template('blog/index.html',rs = rs)

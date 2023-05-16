f8b30e538c82e7aa92b88f5a504485dc45ec0d73
ZZH <13164108610@163.com>
2023-05-16 18:27:24 +0800

    update

--- web/controllers/musicGen/musicgen.py
+++ web/controllers/musicGen/musicgen.py
@@ -4,3 +4,15 @@ from flask import render_template
 @route_musicGen.route('/musicgen',methods=["Get","POST"])
 def musicgen():
     return render_template('musicGen/musicgen.html')
+
+@route_musicGen.route('/compose',methods=["Get","POST"])
+def compose():
+    return render_template('musicGen/compose.html')
+
+@route_musicGen.route('/musicplay')
+def musicplay():
+    return render_template('musicGen/musicplay.html')
+
+@route_musicGen.route('/music')
+def music():
+    return render_template('musicGen/music.html')

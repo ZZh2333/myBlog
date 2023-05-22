1ad5091d8c6203dcfa1fb3e97679717d4b7a9171
ZZH <13164108610@163.com>
2023-05-22 17:50:50 +0800

    update

--- web/controllers/musicGen/api.py
+++ web/controllers/musicGen/api.py
@@ -1,6 +1,31 @@
 from . import route_musicGen
+from flask import request, render_template, jsonify, redirect
+import os
+from datetime import timedelta
+import json
 
 @route_musicGen.route('/api')
 def api():
     ans = 'zzh1'
     return ans
+
+@route_musicGen.route('/upload', methods=['POST', 'GET'])
+def upload():
+    # print("222")
+    if request.method == 'POST':
+        # 通过file标签获取文件
+        f = request.files['img']
+        # if not (f and allowed_file(f.filename)):
+        #     return jsonify({"error": 1001, "msg": "图片类型：png、PNG、jpg、JPG、bmp"})
+        # 当前文件所在路径
+        basepath = os.path.dirname(__file__)
+        # print(basepath)
+        # 一定要先创建该文件夹，不然会提示没有该路径
+        upload_path = os.path.join(basepath, '../../static/musicGen/music/img', '1.jpg')
+        # 保存文件
+        print(upload_path)
+        f.save(upload_path)
+        # 返回上传成功界面
+        return json.dumps("success"),200,{'Content-Type':'application/json'}
+    # 重新返回上传界面
+    return json.dumps("success"),200,{'Content-Type':'application/json'}
--- web/controllers/musicGen/musicgen.py
+++ web/controllers/musicGen/musicgen.py
@@ -1,5 +1,6 @@
 from . import route_musicGen
-from flask import render_template
+from flask import render_template, request
+import numpy as np
 
 @route_musicGen.route('/musicgen',methods=["Get","POST"])
 def musicgen():
@@ -13,6 +14,32 @@ def compose():
 def musicplay():
     return render_template('musicGen/musicplay.html')
 
-@route_musicGen.route('/music')
+@route_musicGen.route('/music',methods=["Get","POST"])
 def music():
+    # model = torch.load('.\motifModel.pth')
+    # x1 = np.random.randint(50,70)
+    # x2 = np.random.randint(25,40)
+    # x3 = np.random.randint(50,80)
+    # x4 = np.random.randint(15,40)
+    # x5 = np.random.randint(80,130)
+    # x = [131] + [x1,x2,x3,x4,x5] + [132] + [130] * 59
+    # x = torch.LongTensor(x)
+    # y = ''.join([" "+ str(i) for i in predict(x.unsqueeze(0))[0].tolist()])
+    # midi_batch = []
+    # for i,mus in enumerate(y.split(" ")):
+    #     if(mus == str(132)):
+    #         break
+    #     if(mus != str(131) and mus != ""):
+    #         midi_batch.append(mus)
+    # midi_batch = midi_batch[1:]
+    # print(midi_batch)
+    # midi_array = []
+    # for i in midi_batch:
+    #     z = [0] * 130
+    #     z[int(i)] = 1
+    #     midi_array.append(z)
+    # numpy_to_midi(torch.LongTensor(midi_array),'./pic9.mid')
+
+
+
     return render_template('musicGen/music.html')

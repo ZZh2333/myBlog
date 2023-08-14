8f668a2e556dc241771fd6deaacc823157959dc4
ZZH <13164108610@163.com>
2023-08-14 14:32:16 +0800

    update

--- web/controllers/blog/__init__.py
+++ web/controllers/blog/__init__.py
@@ -1,15 +1,16 @@
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template, request
 import pandas as pd
 
 route_blog = Blueprint('blog_page',__name__)
 
-from web.controllers.blog.steamcmdinstall import *
-from web.controllers.blog.maoxiandao import *
-from web.controllers.blog.mysql import *
-from web.controllers.blog.shellbash import *
+# from web.controllers.blog.steamcmdinstall import *
+# from web.controllers.blog.maoxiandao import *
+# from web.controllers.blog.mysql import *
+# from web.controllers.blog.shellbash import *
 
 @route_blog.route('/',methods=["Get","POST"])
 def index():
+
     data = pd.read_csv('web/static/blog/csv/flod.csv')
     data['Date'] = pd.to_datetime(data['Date'])
     data['Year'] = data.Date.apply(lambda x: x.year)
@@ -20,4 +21,9 @@ def index():
     rs = []
     for index,row in data.iterrows():
         rs.append(row)
+
+    wanted = request.args.get("wanted", type=str)
+    if wanted:
+        # print("/blog/{}.html"%str(wanted))
+        return render_template("/blog/{}.html".format(wanted),rs = rs)
     return render_template('blog/index.html',rs = rs)

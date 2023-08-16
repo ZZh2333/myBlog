71e4848f4c2358db9fff73dc35aa9a11661de6b6
ZZH <13164108610@163.com>
2023-08-16 17:17:33 +0800

    update

--- web/controllers/index.py
+++ web/controllers/index.py
@@ -5,7 +5,8 @@ route_index = Blueprint('index_page',__name__)
 
 @route_index.route('/')
 def myindex():
-    return render_template("index/myindex.html")
+    # return render_template("index/myindex.html")
+    return render_template("blog/index.html")
 
 
 @route_index.route('/index')

9c424af35d01a279880c55769eca799f699b355c
ZZH <13164108610@163.com>
2023-09-19 15:45:52 +0800

    update

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
-cd ~/myblog && echo "Autoupdate starting......" || exit 1
+cd ~/myblog && echo "Autoupdate starting......"
 echo "Now in `pwd`"
-source /root/anaconda3/bin/activate class_BigData || echo "error source"
+source /root/anaconda3/bin/activate class_BigData || echo "error source!!!"
 uwsgi --stop uwsgi.pid
 git pull -f origin main
 uwsgi uwsgi.ini && echo "success!" || echo "failed!!!!!!!!"

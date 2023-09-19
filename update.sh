e3a06325b9509c5cde92b712f28f724723ec55c7
ZZH <13164108610@163.com>
2023-09-19 16:01:40 +0800

    update

--- update.sh
+++ update.sh
@@ -4,4 +4,5 @@ echo "Now in `pwd`"
 source /root/anaconda3/bin/activate class_BigData || echo "error source!!!"
 uwsgi --stop uwsgi.pid
 git pull -f origin main
+chmod u+x update.sh
 uwsgi uwsgi.ini && echo "success!" || echo "failed!!!!!!!!"

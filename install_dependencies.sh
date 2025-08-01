e1388c6d11d83db37a1d5bc404dc88f4370857f0
ZZH <13164108610@163.com>
2025-08-01 16:30:04 +0800

    update

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,31 +1,23 @@
 #!/bin/bash
 
-
+source ~/.bashrc
 conda activate myblog
 # 检查是否安装了 Python 和 pip
-if ! command -v python3 &> /dev/null; then
-    echo "Python3 未安装，请先安装 Python3。"
+if ! command -v python &> /dev/null; then
+    echo "python 未安装，请先安装 python。"
     exit 1
 fi
 
-if ! command -v pip3 &> /dev/null; then
-    echo "pip3 未安装，正在安装 pip3..."
+if ! command -v pip &> /dev/null; then
+    echo "pip 未安装，正在安装 pip..."
     sudo apt-get update
-    sudo apt-get install -y python3-pip
+    sudo apt-get install -y python-pip
 fi
 
-# 创建虚拟环境
-echo "创建虚拟环境..."
-python3 -m venv venv
-
-# 激活虚拟环境
-echo "激活虚拟环境..."
-source venv/bin/activate
-
-# 安装依赖
+# 安装依赖并跳过 @ 后目录不存在的报错
 if [ -f "requirements.txt" ]; then
     echo "正在安装依赖..."
-    pip3 install -r requirements.txt
+    pip install --ignore-installed --no-deps -r requirements.txt
 else
     echo "未找到 requirements.txt 文件，请确保文件存在。"
     exit 1
